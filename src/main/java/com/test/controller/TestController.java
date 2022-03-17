b80cb0de5da7660dedec4512a1416118b4e7967d
dongzooo <dhel48@nate.com>
2022-03-17 15:50:30 +0900

    upload spring study

--- src/main/java/com/test/controller/TestController.java
+++ src/main/java/com/test/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.test.controller;
 
+
+
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,9 +16,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class TestController {
 	
 	//변수 위에 @Autowired 붙여주면 xml 파일에 <bean>으로 등록된 스프링빈 객체를 찾아서 자동으로 주입해준다.
-	@Autowired
+	// xml파일에 <bean>으로 등록된 스프링빈(생성된 객체)를 찾아서 자동으로 주입해준다.
+	// 객체생성은 -context.xml 파일에 bean으로 등록하고 필요한 곳에서 주입받아 사용가능
+	// 먼저 변수의 타입 먼저 확인해서 주입해줌
+	
+	@Autowired //각각 변수에 하나씩 붙혀주기
 	private Date date;
 
+	@Autowired //각각 변수에 하나씩 붙혀주기
+	private Date day;
+	
 	//#1 리턴타입 : String : return "jsp 파일 이름"
 	@GetMapping("hello")
 	public String hello() {
