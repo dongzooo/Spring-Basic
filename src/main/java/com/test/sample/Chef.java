836f6f4284f3eee04548f458ef39812ac16f94f4
dongzooo <dhel48@nate.com>
2022-03-17 16:47:31 +0900

    upload TIL spring

--- src/main/java/com/test/sample/Chef.java
+++ src/main/java/com/test/sample/Chef.java
@@ -1,5 +1,12 @@
 package com.test.sample;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+@ComponentScan
+@Service
+@Repository
 public class Chef {
 	private String name;
 
